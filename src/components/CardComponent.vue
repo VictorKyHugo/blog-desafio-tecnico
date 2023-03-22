2e84f7d0d92452403c4c762f92101c6696141db1
victorkyhugo <victorkyhugo@gmail.com>
2023-03-22 16:54:25 -0300

    feat: Card styles done

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <img class="card__thumbnail" src="https://picsum.photos/400/200?random=34" />
+    <div class="card__info">
+      <h4 class="card__info__title">
+        eligendi iste nostrum consequuntur adipisci praesentium sit beatae perferendis
+      </h4>
+      <p class="card__info__content">
+        quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae
+        ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto
+      </p>
+
+      <div class="card__info__footer">
+        <div class="card__info__footer__author">
+          <img
+            class="card__info__footer__author__profile-picture"
+            src="https://picsum.photos/32?random=34"
+          />
+          <span class="card__info__footer__author__name">Júlio César</span>
+        </div>
+        <button class="card__info__footer__read-more">Ler Artigo</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>

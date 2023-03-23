320215b589139342090ab8dccb09fbaac4e12bdc
Victor Hugo <kyoouth@gmail.com>
2023-03-23 14:11:12 -0300

    Revert "Feature/Card-Component"

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,8 +1,12 @@
 <template>
-  <div @click="handleReadMore" data-test="card" class="card">
-    <img class="card__thumbnail" src="https://picsum.photos/400/200?random=34" />
+  <div class="card">
+    <img
+      @click="handleReadMore"
+      class="card__thumbnail"
+      src="https://picsum.photos/400/200?random=34"
+    />
     <div class="card__wrapper">
-      <div class="card__wrapper__info">
+      <div @click="handleReadMore" class="card__wrapper__info">
         <h4 class="card__wrapper__info__title">
           <slot name="title"></slot>
         </h4>
@@ -11,27 +15,37 @@
         </p>
       </div>
       <div class="card__wrapper__footer">
-        <div class="card__wrapper__footer__author">
-          <img
-            class="card__wrapper__footer__author__profile-picture"
-            src="https://randomuser.me/api/portraits/thumb/women/12.jpg"
-          />
+        <div @click="handleAuthorClick" class="card__wrapper__footer__author">
+          <div class="card__wrapper__footer__author__profile-picture">
+            {{ profilePicture }}
+          </div>
           <span class="card__wrapper__footer__author__name">{{ author }}</span>
         </div>
-        <button class="card__wrapper__footer__read-more">Ler Artigo</button>
+        <button @click="handleReadMore" class="card__wrapper__footer__read-more">Ler Artigo</button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { computed } from 'vue';
+
+const props = defineProps<{
   author: string;
 }>();
 
 const emit = defineEmits(['onAuthorClick', 'onReadMore']);
 
+const handleAuthorClick = () => {
+  emit('onAuthorClick');
+};
+
 const handleReadMore = () => {
   emit('onReadMore');
 };
+
+const profilePicture = computed(() => {
+  const authorFirstLetter = props.author.split('')[0];
+  return authorFirstLetter;
+});
 </script>

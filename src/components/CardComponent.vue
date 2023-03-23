ddfcff966ad4a59f7359f77e20d232e568bc9635
victorkyhugo <victorkyhugo@gmail.com>
2023-03-22 21:01:37 -0300

    feat: add slots and click events

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,27 +1,51 @@
 <template>
   <div class="card">
-    <img class="card__thumbnail" src="https://picsum.photos/400/200?random=34" />
-    <div class="card__info">
-      <h4 class="card__info__title">
-        eligendi iste nostrum consequuntur adipisci praesentium sit beatae perferendis
-      </h4>
-      <p class="card__info__content">
-        quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae
-        ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto
-      </p>
-
-      <div class="card__info__footer">
-        <div class="card__info__footer__author">
-          <img
-            class="card__info__footer__author__profile-picture"
-            src="https://picsum.photos/32?random=34"
-          />
-          <span class="card__info__footer__author__name">Júlio César</span>
+    <img
+      @click="handleReadMore"
+      class="card__thumbnail"
+      src="https://picsum.photos/400/200?random=34"
+    />
+    <div class="card__wrapper">
+      <div @click="handleReadMore" class="card__wrapper__info">
+        <h4 class="card__wrapper__info__title">
+          <slot name="title"></slot>
+        </h4>
+        <p class="card__wrapper__info__content">
+          <slot name="content"></slot>
+        </p>
+      </div>
+      <div class="card__wrapper__footer">
+        <div @click="handleAuthorClick" class="card__wrapper__footer__author">
+          <div class="card__wrapper__footer__author__profile-picture">
+            {{ profilePicture }}
+          </div>
+          <span class="card__wrapper__footer__author__name">{{ author }}</span>
         </div>
-        <button class="card__info__footer__read-more">Ler Artigo</button>
+        <button @click="handleReadMore" class="card__wrapper__footer__read-more">Ler Artigo</button>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+  author: string;
+}>();
+
+const emit = defineEmits(['onAuthorClick', 'onReadMore']);
+
+const handleAuthorClick = () => {
+  emit('onAuthorClick');
+};
+
+const handleReadMore = () => {
+  emit('onReadMore');
+};
+
+const profilePicture = computed(() => {
+  const authorFirstLetter = props.author.split('')[0];
+  return authorFirstLetter;
+});
+</script>

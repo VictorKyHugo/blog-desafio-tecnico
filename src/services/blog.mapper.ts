5a97e5559c5a5863f9e366331724244ffd695fb8
victorkyhugo <victorkyhugo@gmail.com>
2023-03-23 23:43:04 -0300

    feat: fetch and store api data

---
+++ src/services/blog.mapper.ts
@@ -0,0 +1,67 @@
+interface IPostData {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface IUserData {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+interface IPostComment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+export const mapBlog = ({ users, posts }: { users: Array<IUserData>; posts: Array<IPostData> }) => {
+  return {
+    posts: posts.map(({ id, userId, title, body }) => ({
+      id,
+      userId,
+      title,
+      content: body
+    })),
+    users: users.map(({ id, name, username, email }) => ({
+      id,
+      name,
+      username,
+      email
+    }))
+  };
+};
+
+export const mapPostComments = (comments: Array<IPostComment>) => {
+  return {
+    comments: comments.map(({ body, email, id, name, postId }) => ({
+      id,
+      postId,
+      name,
+      email,
+      body
+    }))
+  };
+};
---
+++ src/services/blog.service.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { mapBlog, mapPostComments } from './blog.mapper';
+
+const initialRoutes = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts/'
+];
+
+export const fetchBlogData = () =>
+  axios.all(initialRoutes.map((promise) => axios.get(promise))).then(
+    axios.spread((users, posts) => {
+      const usersData = users.data;
+      const postsData = posts.data;
+
+      return mapBlog({ users: usersData, posts: postsData });
+    })
+  );
+
+export const fetchPostComments = (postId: number) =>
+  axios
+    .get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+    .then((response) => mapPostComments(response.data));

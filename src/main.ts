37332cb19025373045fda93b64778f122f5ed5bb
victorkyhugo <victorkyhugo@gmail.com>
2023-03-24 15:58:26 -0300

    feat: install vue paginate

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import VueAwesomePaginate from 'vue-awesome-paginate';
+import 'vue-awesome-paginate/dist/style.css';
 
 import App from './App.vue';
 import router from './router';
@@ -10,5 +12,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueAwesomePaginate);
 
 app.mount('#app');

3b456334369ed0edb3f4118140fa8c1dca3d18de
victorkyhugo <victorkyhugo@gmail.com>
2023-03-25 19:01:56 -0300

    feat: add back icon in Header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="header">
+    <BackIcon
+      v-if="!isHomePage"
+      data-test="icon"
+      class="header__back-icon"
+      @click="backToLastPage"
+    />
     <h1 @click="backToHomePage" data-test="logo" class="header__logo">BlogMock</h1>
   </div>
   <div class="header__hr"></div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import { computed } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import BackIcon from './icons/BackIcon.vue';
 
 const router = useRouter();
+const route = useRoute();
+
+const isHomePage = computed(() => route.name === 'home');
+
+const backToLastPage = (): void => {
+  router.back();
+};
 
 const backToHomePage = (): void => {
   router.push('/');

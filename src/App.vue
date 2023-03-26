0da6a0621c4e22c0c88d75fc5ffe0c1a5aa5a8dd
victorkyhugo <victorkyhugo@gmail.com>
2023-03-25 22:45:07 -0300

    feat: added page loading

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <Suspense>
-    <router-view :key="route.fullPath"></router-view>
-  </Suspense>
+  <Header />
+  <RouterView :key="route.fullPath" v-slot="{ Component }">
+    <template v-if="Component">
+      <Transition mode="out-in">
+        <KeepAlive>
+          <Suspense>
+            <component :is="Component"></component>
+            <template #fallback> Loading... </template>
+          </Suspense>
+        </KeepAlive>
+      </Transition>
+    </template>
+  </RouterView>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import Header from './components/HeaderComponent.vue';
 const route = useRoute();
 </script>
